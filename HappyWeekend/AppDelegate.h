c8bdcf8315ef7571ebdf83ddc4a0804ddc2e5148
聂欣欣 <1498146887@qq.com>
2016-01-04 12:08:49 +0800

    first commit

---
+++ HappyWeekend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HappyWeekend
+//
+//  Created by wanghongxiao on 16/1/4.
+//  Copyright © 2016年 聂欣欣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
